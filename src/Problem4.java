bc151bb881de1413599748af646676c053408c08
evagabriela <gabriela.osu@gmail.com>
2014-05-21 15:46:17 -0700

    refactor and clean code for Problem 4

--- src/Problem4.java
+++ src/Problem4.java
@@ -12,17 +12,13 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 
     public String run() {
         ArrayList<Integer> palindromeList = palindromeNumber();
-
         int max = palindromeList.get(0);
-
         for (int i = 1; i < palindromeList.size(); i ++){
             if (palindromeList.get(i) > max){
                max = palindromeList.get(i);
             }
         }
-        String maxNumber = String.valueOf(max);
-
-        return maxNumber;
+        return String.valueOf(max);
     }
 
     protected ArrayList<Integer> palindromeNumber(){
@@ -33,7 +29,6 @@ Find the largest palindrome made from the product of two 3-digit numbers.
         for (int i = 999; i > 100; i--){
             for (int j = 999; j > 100; j--){
                 a = i * j;
-
                 if (isPaling(a)){
                     palindrome.add( i * j);
                 }
@@ -43,22 +38,13 @@ Find the largest palindrome made from the product of two 3-digit numbers.
     }
 
     protected boolean isPaling(int number) {
-
         String numberString = String.valueOf(number);
 
         String reverse = "";
         int length = numberString.length();
-
         for( int i = length - 1 ; i >= 0 ; i-- ) {
             reverse = reverse + numberString.charAt(i);
         }
-
-        if (reverse.equals(numberString)){
-
-            return true;
-        }
-        return false;
+        return reverse.equals(numberString);
     }
-
-
 }
