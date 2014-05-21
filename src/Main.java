3ddc13e5bc6ef13c7cd050e9bff81809c7c74338
evagabriela <gabriela.osu@gmail.com>
2014-05-21 15:02:05 -0700

    add problem 4 solution to find the largest palindrome made from teh product of two 3-digit numbers

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,10 @@ public class Main {
         Problem3 largestPrimeFactor = new Problem3();
         String problem3Solution = largestPrimeFactor.run(600851475143L);
         System.out.println(problem3Solution + " is the largest prime factor for the number 600851475143");
+
+//Problem 4:
+        Problem4 largestPalindrome = new Problem4();
+        String problem4Solution = largestPalindrome.run();
+        System.out.println(problem4Solution + " is the largest palindrome made from the product of two 3-digit numbers");
     }
 }
---
+++ src/Problem4.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+
+/**
+ * Created by gzamudio on 5/21/14.
+ */
+public class Problem4 {
+    /*
+A palindromic number reads the same both ways.
+ex: The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+    public String run() {
+        ArrayList<Integer> palindromeList = palindromeNumber();
+
+        int max = palindromeList.get(0);
+
+        for (int i = 1; i < palindromeList.size(); i ++){
+            if (palindromeList.get(i) > max){
+               max = palindromeList.get(i);
+            }
+        }
+        String maxNumber = String.valueOf(max);
+
+        return maxNumber;
+    }
+
+    private ArrayList<Integer> palindromeNumber(){
+
+        ArrayList<Integer> palindrome = new ArrayList<Integer>();
+        int a;
+
+        for (int i = 999; i > 100; i--){
+            for (int j = 999; j > 100; j--){
+                a = i * j;
+
+                if (isPaling(a)){
+                    palindrome.add( i * j);
+                }
+            }
+        }
+        return palindrome;
+    }
+
+    private boolean isPaling(int number) {
+
+        String numberString = String.valueOf(number);
+
+        String reverse = "";
+        int length = numberString.length();
+
+        for( int i = length - 1 ; i >= 0 ; i-- ) {
+            reverse = reverse + numberString.charAt(i);
+        }
+
+        if (reverse.equals(numberString)){
+
+            return true;
+        }
+        return false;
+    }
+
+
+}
