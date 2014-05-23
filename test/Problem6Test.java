844084b302a97d3356c338f8236f3a0ebe4a2c09
evagabriela <gabriela.osu@gmail.com>
2014-05-23 12:52:34 -0700

    add test and code for the problem 6 of project euler

--- test/Problem6Test.java
+++ test/Problem6Test.java
@@ -1,12 +1,41 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class Problem6Test {
+    private Problem6 problem6;
+
+    @Before
+    public void setUp(){
+        problem6 = new Problem6();
+
+    }
+
     @Test
     public void shouldReturnProblemResultAsAString(){
-        Problem6 problem6 = new Problem6();
         assertEquals("25164150", problem6.run());
+    }
+
+    @Test
+    public void shouldReturnTheSquareOfTheSumOfTheFirstTenNaturalNumbers(){
+        assertEquals(25502500, problem6.squareOfTheSumOfTheFirstTenNaturalNumbers());
+
+    }
+
+    @Test
+    public void shouldReturnTheDifferenceBetweenTheSumOfTheSquareOfTheFirstTenNumbersAndTheSquareOfTheSumOfThem(){
+        assertEquals(25164150, problem6.differenceBetweenBoth());
+    }
+
+    @Test
+    public void shouldReturnTheSumOfTheSquaresOfTheFirstTenNumbers(){
+        assertEquals(338350, problem6.sumOfTheSquaresOfFirstTenNaturalNumbers());
+    }
+
+    @Test
+    public void shouldReturnTheSquareOfTheSumOfTheFirstTenNumbers(){
+        assertEquals(25502500, problem6.squareOfTheSumOfTheFirstTenNaturalNumbers());
     }
 
 }
